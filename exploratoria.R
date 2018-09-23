b88277e2203008d1b696ba77b6deed81dfa95f05
lukezheng2 <30845182+lukezheng2@users.noreply.github.com>
2018-09-23 00:34:10 -0300

    Add files via upload

--- exploratoria.R
+++ exploratoria.R
@@ -1,13 +1,26 @@
 library(ggplot2)
+library(reshape2)
+library(stringr)
+load("dados.rda")
+
+dados %<>% cbind(str_split_fixed(dados$parcela, "", 2))
+
+names(dados)[14] <- "Parcela2"
+names(dados)[15] <- "Replicacao"
 
 # Discritivas -------------------------------------------------------------
+#Percentuais
+dados %>% 
+  select(estrutura) %>% 
+  table %>% as.data.frame() %>% 
+  mutate(Perc = Freq/nrow(dados))
 
 #Perimetro dos troncos
 dados %>% 
   mutate(Cat_diametro = ifelse(diametro<5, "(2-5)", NA),
          Cat_diametro = ifelse(diametro<10 & diametro>=5, "(5-10)", Cat_diametro),
          Cat_diametro = ifelse(diametro>=10, "(+10)", Cat_diametro)) %>% 
-  select(Cat_diametro) %>% 
+  select(Cat_diametro, decomp) %>% 
   table %>% as.data.frame() %>% 
   mutate(Perc = Freq/sum(Freq))
 
@@ -61,42 +74,65 @@ dados %>%
 
 #Perimetro dos troncos
 
-dados %>% 
+p1 <- dados %>% 
   mutate(Cat_diametro = ifelse(diametro<5, "(2-5)", NA),
          Cat_diametro = ifelse(diametro<10 & diametro>=5, "(5-10)", Cat_diametro),
          Cat_diametro = ifelse(diametro>=10, "(+10)", Cat_diametro)) %>% 
-         {ggplot(., aes(x = parcela,
+         {ggplot(., aes(x = Parcela2,
                         group = factor(Cat_diametro, levels = c("(2-5)", "(5-10)", "(+10)")),
                         fill = factor(Cat_diametro, levels = c("(2-5)", "(5-10)", "(+10)")))) +
              geom_bar(color="black", position = "fill") +
              #theme(legend.position = "none") +
-             labs(x = "Diâmetro" , y = "Percentual") +
-             scale_fill_brewer(palette = "Dark2", "Diâmetro")
+             labs(x = "Parcela" , y = "Percentual") +
+             scale_fill_brewer(palette = "Dark2", "Diâmetro (cm)")
          }
 
 # Decomposição 
 
-dados %>% 
-  ggplot(aes(x = parcela, group = decomp, fill = decomp)) +
+p2 <- dados %>% 
+  ggplot(aes(x = Parcela2, group = decomp, fill = decomp)) +
   geom_bar(color="black", position = "fill") +
-  labs(x = "" , y = "Percentual") +
+  labs(x = "Parcela" , y = "Percentual") +
   scale_fill_brewer(palette = "Dark2", "Grau de decomposição")
 
 
 
 # Estrutura
 
-dados %>% 
-  ggplot(aes(x = parcela, group = estrutura, fill = estrutura)) +
+p3 <- dados %>% 
+  ggplot(aes(x = Parcela2, group = estrutura, fill = estrutura)) +
   geom_bar(color="black", position = "fill") +
   #theme(legend.position = "none") +
-  labs(x = "" , y = "Percentual") +
+  labs(x = "Parcela" , y = "Percentual") +
   scale_fill_brewer(palette = "Dark2", "Estrutura")
 
 # Podridao 
 
-dados %>% 
-  ggplot(aes(x = parcela, group = podridao, fill = podridao)) +
+p4 <- dados %>% 
+  ggplot(aes(x = Parcela2, group = podridao, fill = podridao)) +
   geom_bar(color="black", position = "fill") +
+  labs(x = "Parcela" , y = "Percentual") +
+  scale_fill_brewer(palette =  "Dark2", "Podridão")
+
+gridExtra::grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+# Tipo de estrutura por decomposição
+
+dados %>% 
+  ggplot(aes(x = estrutura, group = podridao, fill = podridao)) +
+  geom_bar(color="black", position = position_dodge()) +
   labs(x = "" , y = "Percentual") +
   scale_fill_brewer(palette =  "Dark2", "Podridão")
+
+#Decomposição x Diametro
+dados %>% 
+  mutate(Cat_diametro = ifelse(diametro<5, "(2-5)", NA),
+         Cat_diametro = ifelse(diametro<10 & diametro>=5, "(5-10)", Cat_diametro),
+         Cat_diametro = ifelse(diametro>=10, "(+10)", Cat_diametro)) %>% 
+         {ggplot(., aes(x = decomp,
+                        group = factor(Cat_diametro, levels = c("(2-5)", "(5-10)", "(+10)")),
+                        fill = factor(Cat_diametro, levels = c("(2-5)", "(5-10)", "(+10)")))) +
+             geom_bar(color="black") +
+             #theme(legend.position = "none") +
+             labs(x = "Grau de decomposição" , y = "Frequência") +
+             scale_fill_brewer(palette = "Dark2", "Diâmetro (cm)")}
