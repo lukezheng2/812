2d68bd83715f6f49598dd682801d52a13556f83e
sedr555 <33204008+sedr555@users.noreply.github.com>
2018-10-15 23:06:18 -0300

    Add files via upload

--- anova.R
+++ anova.R
@@ -8,13 +8,13 @@
   grausDeg = data.frame(decomp = factor(1:5), densidade = c(0.40, 0.30, 0.19, 0.13, 0.09))
 
 # Estimando o volume ----
-  volumesGrau = dados %>% group_by(tratamento, replicacao, linha, decomp, ano = year(data)) %>%
+  massaGrau = dados %>% group_by(tratamento, replicacao, linha, decomp, ano = year(data)) %>%
               summarise(volumeTotal = pi^2/(8*45) * sum(diametro^2))
   
-  volumesGrau = inner_join(volumesGrau, grausDeg) %>% mutate(massa = densidade * volumeTotal)
+  massaGrau = inner_join(massaGrau, grausDeg) %>% mutate(massa = densidade * volumeTotal)
   
-  volumesTotal = volumesGrau %>% group_by(tratamento, replicacao, linha, ano) %>% summarise(massa = sum(massa))
+  massaTotal = massaGrau %>% group_by(tratamento, replicacao, linha, ano) %>% summarise(massa = sum(massa))
 
 # ANOVA ----
-  aov(massa ~ tratamento, data = volumesTotal) %>% anova()
+  aov(massa ~ tratamento, data = massaTotal) %>% anova()
   
