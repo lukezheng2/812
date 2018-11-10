b8f7514a0d3cd51063d810a03134fec7dac57a9f
hgmv13 <43380497+hgmv13@users.noreply.github.com>
2018-11-09 22:39:51 -0200

    Add files via upload

---
+++ Respostas.R
@@ -0,0 +1,12 @@
+##extrair variável resposta
+
+#antes de 2018
+ respostaAnt = ant %>%
+  group_by(linha, tratamento, replicacao, decomp, podridao) %>% summarise(vol = pi * sum(diametro)/(8*45))
+ 
+ #2018
+ respostaPos = pos %>%
+   group_by(linha, tratamento, replicacao, decomp, podridao) %>% summarise(vol = pi * sum(diametro)/(8*45))
+######### Talvez tirar a podridao pois tem mto NA
+ 
+ 
