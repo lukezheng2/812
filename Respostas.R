c7ee0e19d22cd390091fadb8e9fa74240b257f4d
hgmv13 <43380497+hgmv13@users.noreply.github.com>
2018-11-09 22:44:50 -0200

    Update Respostas.R

--- Respostas.R
+++ Respostas.R
@@ -2,11 +2,11 @@
 
 #antes de 2018
  respostaAnt = ant %>%
-  group_by(linha, tratamento, replicacao, decomp, podridao) %>% summarise(vol = pi * sum(diametro)/(8*45))
+  group_by(linha, tratamento, replicacao, decomp, podridao, ano) %>% summarise(vol = pi * sum(diametro)/(8*45))
  
  #2018
  respostaPos = pos %>%
-   group_by(linha, tratamento, replicacao, decomp, podridao) %>% summarise(vol = pi * sum(diametro)/(8*45))
+   group_by(linha, tratamento, replicacao, decomp, podridao, ano) %>% summarise(vol = pi * sum(diametro)/(8*45))
 ######### Talvez tirar a podridao pois tem mto NA
  
  
